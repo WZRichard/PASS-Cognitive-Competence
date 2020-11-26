d9e8bf62fb290282e166513ed0d554755fe5ae25
WZRichard <31801137@stu.edu.edu.cn>
2020-11-26 08:45:07 +0800

    题库&随机

--- pages/FruitMatch/FruitMatch.js
+++ pages/FruitMatch/FruitMatch.js
@@ -5,38 +5,50 @@ Page({
    * 页面的初始数据
    */
   data: {
+    cnt_row: 2,
+    round: 1,
+    difficulty: [2, 2, 3, 3, 4, 4, 5, 6],
     row: [],
-    src: ['./data/apple.png', './data/banana.png']
+    src: ['./data/apple.png', './data/banana.png', './data/grape.png',
+      './data/pear.png', './data/strawberry.png', './data/watermelon.png'],
+
+  },
+
+  /**
+   * 游戏相关函数
+   */
+
+  set_row: function () {
+    var new_row = [[], [], []];
+    for (let i = 0; i < this.data.cnt_row; i++) {
+      for (let j = 0; j < this.data.cnt_row; j++) {
+        new_row[i].push(this.data.src[this.getRadom()]);
+      }
+    }
+    this.setData({
+      row: new_row,
+    })
+  },
+
+  getRadom: function () {
+    return parseInt(Math.random() * this.data.difficulty[this.data.round - 1]);
+  },
+
+  tap_image: function (e) {
+    console.log(e.currentTarget.dataset.num);
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.set_row()
-  },
-
-  set_row: function () {
-    this.setData({
-      row: [
-        [this.data.src[0], this.data.src[1]],
-        [this.data.src[0], this.data.src[1]],
-      ],
-    })
-  },
-
-  /**
-   * 游戏内部函数
-   */
-  tap_image: function (e) {
-    console.log(e.currentTarget.dataset.num);
+    this.set_row();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
   },
 
   /**
